9ac95ad8461efe2b3d2043af2fc9456cfc2280da
Akimori <wee.bscl@gmail.com>
2023-04-29 16:12:11 +0800

    add instrument type

--- src/app/models/instrument.ts
+++ src/app/models/instrument.ts
@@ -1,9 +1,11 @@
 import { User } from "./user"
 
 export interface Instrument {
+    instrument_id: number
+    type: string
     brand: string
     model: string
     serial_number: string
     store_name: string
-    user: User
+    user?: User
 }
