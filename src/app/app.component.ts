e4a2702551d155d4f928f07fa878f4f61cca3318
Akimori <wee.bscl@gmail.com>
2023-05-13 19:14:44 +0800

    edit title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'google-auth';
+  title = 'Instrument Store Management System';
 }
